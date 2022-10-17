3f779a434ecdba7906a342034460ae5a8fb80aae
kola192 <mutwakil96@gmail.com>
2022-10-17 23:02:59 +0200

    how to use puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#inclue <stdio.h>
+
+/**
+  * main - Entry point
+  * Return: always return 0
+ */
+int main(void) 
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;
+}
