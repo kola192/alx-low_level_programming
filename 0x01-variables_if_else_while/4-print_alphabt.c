88d30d5a606eb9a88345fa40143f19afffc16ec4
kola192 <mutwakil96@gmail.com>
2022-10-18 14:10:46 +0200

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z', ch++;)
+	{
+		if (ch == 'q' || ch == 'e')
+		{
+			continue;
+		}
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
